817e2efa5fb5e3a177212e2f384029f2425f4d37
Francisco Sanguineti Arena <fsanguineti@itba.edu.ar>
2020-06-01 22:37:31 +0200

    icarl in the init

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from .resnet import resnet20, resnet32, resnet56
+from .iCaRL import iCarl
 
-__all__ = ('resnet20', 'resnet32', 'resnet56')
+__all__ = ('resnet20', 'resnet32', 'resnet56',
+           'iCarl')
