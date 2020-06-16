1fc9eeff24011bc00d214d01cf34fe1f0b443a49
Francisco Sanguineti Arena <fsanguineti@itba.edu.ar>
2020-06-16 18:25:50 +0200

    trying with all the fc layer ready for all the classes

--- trainers/lwf_sequential_train.py
+++ trainers/lwf_sequential_train.py
@@ -60,13 +60,14 @@ def lwf_sequential_train(net, split_datasets, criterion, optimizer_factory,
             # Update network fc layer with more outputs
             n = len(set(train_dataloader.dataset.targets))
             n_classes += n
+            """            
             in_features = net.fc.in_features
             out_features = net.fc.out_features
             weight = net.fc.weight.data
 
             net.fc = nn.Linear(in_features, n_classes, bias=False)
             net.fc.weight.data[:out_features] = weight
-
+            """
             previous_model = copy.deepcopy(net)
             previous_model.train(False)
 
