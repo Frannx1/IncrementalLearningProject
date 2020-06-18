1bf917758a5c01c8ee68a563d73fbbda912b808a
Francisco Sanguineti Arena <fsanguineti@itba.edu.ar>
2020-06-18 11:36:54 +0200

    other loss

--- models/iCaRL.py
+++ models/iCaRL.py
@@ -14,7 +14,8 @@ from datasets.common_datasets import SimpleDataset
 from models.incremental_base import MultiTaskLearner
 from models.resnet import get_resnet
 from models.utils import l2_normalize
-from models.utils.utilities import timer, remove_row, class_dist_loss_icarl, ReverseIdxSorted
+from models.utils.utilities import timer, remove_row, class_dist_loss_icarl, ReverseIdxSorted, \
+    classification_and_distillation_loss
 
 
 class iCaRL(MultiTaskLearner):
@@ -165,7 +166,7 @@ class iCaRL(MultiTaskLearner):
             for param in self.previous_model.parameters():
                 param.requires_grad = False
 
-            print('adding {} classes, total {}'.format(n, self.n_classes))
+            print('Adding {} classes, total {}'.format(n, self.n_classes))
 
     def train_task(self, train_loader, optimizer, scheduler, num_epochs, val_loader=None, log_dir=None):
         self.to(Config.DEVICE)  # this will bring the network to GPU if DEVICE is cuda
@@ -199,7 +200,7 @@ class iCaRL(MultiTaskLearner):
                 if self.previous_model is not None:
                     previous_output = self.previous_model(images)
 
-                loss = class_dist_loss_icarl(
+                loss = classification_and_distillation_loss(
                         outputs,
                         labels,
                         previous_output=previous_output,
