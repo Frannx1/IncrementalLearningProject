87bccfaa0fb3c61e9ff09840620c927e9c3fa44c
Francisco Sanguineti Arena <fsanguineti@itba.edu.ar>
2020-06-22 00:30:56 +0200

    lwf in modules

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .resnet import resnet20, resnet32, resnet56
 from .iCaRL import iCaRL
+from .lwf import LwF
 
 __all__ = ('resnet20', 'resnet32', 'resnet56',
-           'iCaRL')
+           'iCaRL', 'LwF')
