2339467a5cfa78425fad95fbafa242264f4f254a
Francisco Sanguineti Arena <fsanguineti@itba.edu.ar>
2020-06-22 00:49:59 +0200

    simple learner

---
+++ models/simple_learner.py
@@ -0,0 +1,29 @@
+from torch import nn
+
+from models.incremental_base import MultiTaskLearner
+from models.resnet import get_resnet
+from models.utils.utilities import classification_and_distillation_loss
+
+
+class SimpleLearner(MultiTaskLearner):
+
+    def __init__(self, resnet_type="32", num_classes=10):
+        super(SimpleLearner, self).__init__(resnet_type=resnet_type, num_classes=num_classes)
+
+        self.features_extractor = get_resnet(resnet_type)
+        self.features_extractor.fc = nn.Sequential()
+
+    def forward_and_compute_loss(self, images, labels):
+        # Forward pass to the network
+        outputs = self(images)
+
+        loss = classification_and_distillation_loss(    # It just use the classification loss
+            outputs,
+            labels,
+        )
+        return outputs, loss
+
+    def before_task(self, train_loader, targets, val_loader=None, use_bias=False):
+        super().before_task(train_loader, targets, val_loader, use_bias)
+
+
