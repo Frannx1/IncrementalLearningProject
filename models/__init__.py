a172e8bdb72faff207732f722b163bc2dae65d37
Francisco Sanguineti Arena <fsanguineti@itba.edu.ar>
2020-06-22 00:51:58 +0200

    simple learner to module

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from .resnet import resnet20, resnet32, resnet56
 from .iCaRL import iCaRL
 from .lwf import LwF
+from .simple_learner import SimpleLearner
 
 __all__ = ('resnet20', 'resnet32', 'resnet56',
-           'iCaRL', 'LwF')
+           'iCaRL', 'LwF', 'SimpleLearner')
