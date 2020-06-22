fbc8d072e607d84fa2bf37190e5ddc6d420210de
Francisco Sanguineti Arena <fsanguineti@itba.edu.ar>
2020-06-22 19:01:27 +0200

    loss in modules

--- models/utils/__init__.py
+++ models/utils/__init__.py
@@ -1,7 +1,9 @@
 from .optimizer import SDGOptimizerAllFactory
 from .scheduler import StepLRSchedulerFactory, MultiStepLRSchedulerFactory
 from .utilities import l2_normalize
+from .loss import DoubleLossBuilder, DoubleLoss, Loss
 
 __all__ = ('SDGOptimizerAllFactory',
            'StepLRSchedulerFactory', 'MultiStepLRSchedulerFactory',
-           'l2_normalize',)
+           'l2_normalize',
+           'DoubleLossBuilder', 'DoubleLoss', 'Loss')
