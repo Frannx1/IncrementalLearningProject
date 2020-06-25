6c8dec90a21161dd2dc2cee096505d8fa37d842b
Francisco Sanguineti Arena <fsanguineti@itba.edu.ar>
2020-06-26 01:35:26 +0200

    add to module icarl selection

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,8 @@ from .resnet import resnet20, resnet32, resnet56
 from .iCaRL import iCaRL
 from .lwf import LwF
 from .simple_learner import SimpleLearner
-from example_usage.test_icarl_extended import iCaRLExtended
+from .iCaRL_extended import iCaRLExtended
+from .iCaRL_selection import iCaRLSelection
 
 __all__ = ('resnet20', 'resnet32', 'resnet56',
-           'iCaRL', 'LwF', 'SimpleLearner', 'iCaRLExtended')
+           'iCaRL', 'LwF', 'SimpleLearner', 'iCaRLExtended', 'iCaRLSelection')
